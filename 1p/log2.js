05350bae70780b7356fa4608d4d7dc92a8f2d9f7
JonathanSolisCeballos On DCompLab <JonathanSolisc_22@hotmail.com>
2019-08-12 19:33:07 -0500

    terminado los ejercicio

--- 1p/log2.js
+++ 1p/log2.js
@@ -1,3 +1,18 @@
-for(let s=1;s<=10;s++){
-	console.log(`Log2(${s}) = ${Math.log2(s)}`)
+/**
+ * Prints the table of log2 from 1 to a number given
+ * @param upTo {number} 
+ * @returns 
+ */
+const LogTable = (base, upTo) => {
+	let arr = [];
+	for(let s=1;s<=upTo;s++){
+		arr.push(`Log${base}(${s})= ${logN(base,s)}`);
+	}
+	return arr;
 }
+
+const logN = (base, number) =>{
+	return Math.log(number) / Math.log(base);
+}
+
+console.table(LogTable(2, 10));
