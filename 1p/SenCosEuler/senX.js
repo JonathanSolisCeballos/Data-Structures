fa98b15107553501771a85ce281de3182b0d4e5c
JonathanSolisCeballos On DCompLab <JonathanSolisc_22@hotmail.com>
2019-08-22 19:38:05 -0500

    Empezado el repetido

--- 1p/SenCosEuler/senX.js
+++ 1p/SenCosEuler/senX.js
@@ -1,7 +1,7 @@
 const senX = (x, upTo) =>{
     let sum = 0;
-    for(let i =0;i<=10;i++){
-        
+    for(let i =1;i<=10;i+=2){
+
     }
 }
 
---
+++ 1p/repetidos.js
@@ -0,0 +1,7 @@
+var arr = [git 1,2,3,4,5,6,7,8,8];
+
+function repetidos(arr){
+    for(let i=0;i<arr.length;i++){
+        
+    }
+}
