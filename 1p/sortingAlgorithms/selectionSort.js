1a0405b73f469d77166a1647422ce85cdf760ecf
JonathanSolisCeballos On DCompLab <JonathanSolisc_22@hotmail.com>
2019-09-02 19:39:55 -0500

    pushed references

---
+++ 1p/sortingAlgorithms/selectionSort.js
@@ -0,0 +1,9 @@
+const selectionSort = (arr) => {
+    let newArr = [...arr];
+    for(let i = 0; i < arr.length ; i++){
+
+    }
+    return newArr;
+}
+const arr = [43,4,6,8,5,3,4,6,34,2,8];
+console.log(selectionSort())
