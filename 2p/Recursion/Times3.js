a8ce9f81f767d2db3eac8b04db2456359e5b56c3
JonathanSolisCeballos On DCompLab <JonathanSolisc_22@hotmail.com>
2019-09-26 19:41:25 -0500

    hoa

---
+++ 2p/Recursion/Times3.js
@@ -0,0 +1,20 @@
+class Times3{
+    constructor(){
+
+    }
+
+    recursion(n){
+        return (n===0) ? 3: (2*this.recursion(n-1) + 3)
+    }
+
+    makeTable(n){
+        let arr = [];
+        for(let i = 0;i<n;i++){
+            arr.push(this.recursion(i))
+        }
+        return arr;
+    }
+}
+
+let a = new Times3();
+console.table(a.makeTable(10));
