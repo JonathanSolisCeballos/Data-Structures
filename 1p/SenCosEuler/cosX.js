48ea380023a50008d79eb71e44b83abafbf5e00f
JonathanSC <jonathansolisc_22@hotmail.com>
2019-09-30 16:54:48 -0500

    Added some

--- 1p/SenCosEuler/cosX.js
+++ 1p/SenCosEuler/cosX.js
@@ -9,7 +9,6 @@ const alt = (i) => {
 const cosX = (x, upTo) =>{
     let sum = 1;
     for(let i = 2,c = 2; c<= upTo;i+=2,c++){
-      // console.log(sum)
       sum += (alt(c) * (pow(x,i) / factorial(i)));
     }
     return sum;
