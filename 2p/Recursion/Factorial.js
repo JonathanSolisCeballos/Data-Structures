4d2511ca27ea1d281b84461688db5d2f5e8d9efb
JonathanSC <jonathansolisc_22@hotmail.com>
2019-09-30 17:52:32 -0500

    Finished fibonacci with recursion

--- 2p/Recursion/Factorial.js
+++ 2p/Recursion/Factorial.js
@@ -1,15 +1,11 @@
-const factorial = n => n < 2 ? 1 : n * factorial(n - 1);
+const factorial = n => (n < 2 ? 1 : n * factorial(n - 1));
+// console.log(factorial(5))
 
 class Factorial {
-    constructor(){
-        this.cost = {};
-    }
-    getValue(n) {
-       // this.cost.['fib(n)'] = 5;
-        return n < 2 ? 1 : n * factorial(n - 1)
-    }
+  getValue(n) {
+    return n < 2 ? 1 : n * factorial(n - 1);
+  }
 }
 
 let fact = new Factorial();
-console.log(fact.getValue(5))
-console.log(factorial(5))
+console.log(fact.getValue(5));
--- 2p/Recursion/Times3.js
+++ 2p/Recursion/Times3.js
@@ -1,19 +1,15 @@
-class Times3{
-    constructor(){
+class Times3 {
+  recursion(n) {
+    return n === 0 ? 3 : 2 * this.recursion(n - 1) + 3;
+  }
 
+  makeTable(n) {
+    let arr = [];
+    for (let i = 0; i < n; i++) {
+      arr.push(this.recursion(i));
     }
-
-    recursion(n){
-        return (n===0) ? 3: (2*this.recursion(n-1) + 3)
-    }
-
-    makeTable(n){
-        let arr = [];
-        for(let i = 0;i<n;i++){
-            arr.push(this.recursion(i))
-        }
-        return arr;
-    }
+    return arr;
+  }
 }
 
 let a = new Times3();
