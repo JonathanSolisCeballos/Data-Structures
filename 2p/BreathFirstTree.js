e22e34ca4e7d2054cb8831091e918178874bb637
JonathanSolisCeballos On DCompLab <JonathanSolisc_22@hotmail.com>
2019-11-07 18:13:32 -0600

    Added binary transversal breath first

--- 2p/BreathFirstTree.js
+++ 2p/BreathFirstTree.js
@@ -1,6 +1,5 @@
-
 //Binary Tree
-(function() {
+(function () {
   class Node {
     constructor(data) {
       this.data = data;
@@ -16,22 +15,22 @@
   let node5 = new Node("5");
   let node6 = new Node("6");
   let node7 = new Node("7");
-  
+
   node1.left = node2;
   node1.right = node3;
   node2.left = node6;
   node2.right = node7;
   node3.left = node4;
   node3.right = node5;
-  
 
-  const breathFirst = (node)=>{//Preorder NLR
+
+  const breathFirst = (node) => { //Preorder NLR
 
     console.log(node.data);
 
-    if(node.left) printTree(node.left)
-    
-    if(node.right) printTree(node.right)
+    if (node.left) printTree(node.left)
+
+    if (node.right) printTree(node.right)
   }
 
   breathFirst(node1);
